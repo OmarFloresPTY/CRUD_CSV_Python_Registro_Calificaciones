768e6895b3f23b84e8cdfabb482cb16985070c1b
Omar Flores <oafo2699@gmail.com>
2023-05-05 21:53:27 -0500

    añadimos el pycache en el gitignore y completamos todas las funciones básicas del CRUD

---
+++ app/CRUD/update_csv.py
@@ -0,0 +1,20 @@
+import csv
+from read_csv import read_csv
+#Es importante poner un try-cast evaluando que la llave no existe en el diccionario. Un keyerror!
+def modify_csv(path,key='Nombre',value='Omar'):
+    data = read_csv(path)
+    for dicc in data:
+        if dicc['CIP'] == '6-722-2106':
+            dicc[key] = value
+    labels = list(data[0].keys())
+    data = list(map(lambda dicc:list(dicc.values()),data))
+    data.insert(0,labels)
+    
+    with open(path,"w",newline='') as csvfile:   
+        writer = csv.writer(csvfile)
+        for lista in data:
+            writer.writerow(lista)
+
+if __name__ == '__main__':
+    path = './app/CSV/Notas_Universitarias.csv'
+    modify_csv(path)
