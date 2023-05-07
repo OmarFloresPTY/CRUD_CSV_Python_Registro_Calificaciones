cadf29980b8018fc26919f92cdccef480ca5a493
Omar Flores <oafo2699@gmail.com>
2023-05-06 20:58:10 -0500

    Agregamos una list comprehensions en update_csv.py, para la variable labels

--- app/CRUD/update_csv.py
+++ app/CRUD/update_csv.py
@@ -7,7 +7,7 @@ def modify_csv(path,key='Nombre',value='Omar'):
     for dicc in data:
         if dicc['CIP'] == '6-722-2106':
             dicc[key] = value
-    labels = list(data[0].keys())
+    labels = [columns for columns in data[0].keys()]
     data = list(map(lambda dicc:list(dicc.values()),data))
     for lista in data:
         prom = reduce(lambda a,b:int(a)+int(b),lista[3:6])//len(lista[3:6])
